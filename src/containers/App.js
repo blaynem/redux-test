642ed3bc12c21fb612167e6ab6fcc4b2b281c4ad
Blayne Marjama <blayne@audioglobe.com>
2017-10-15 21:27:20 -0700

    removed axios from file

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { testThis } from '../actions'
-import axios from 'axios';
 
 class App extends Component {
   testThis = () => {
