8338d00ec80f9e0fec3da9c8ebf03bc92ac2262f
Blayne Marjama <blayne.marjama@gmail.com>
2018-01-04 21:18:23 -0800

    updated actions with timeout

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,17 +3,24 @@ import { MESSAGE, SUBMITFORM } from '../constants'
 // Used to submit the form. Accepts an object with properties of firstName and lastName
 export function formSubmit({firstName, lastName}) {
   return(dispatch) => {
+    // change message to loading immediately
     dispatch(changeMessage("Loading..."))
+    // waits 1.5 seconds the changes the message as well as submits the form to create a sort of api call feel.
     setTimeout(function() {
       dispatch(changeMessage("Done!"))
       dispatch({
         type: SUBMITFORM,
         payload: { firstName, lastName }
       })
+      // 2 seconds after submission, will clear the message
+      setTimeout(function() {
+        dispatch(changeMessage(""))
+      }, 2000);
     }, 1500);
   }
 }
 
+// accepts a payload of type 'string', which will be used to display a message
 export function changeMessage(payload) {
   return (dispatch) => {
     dispatch({
