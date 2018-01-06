856c11a96fce9dfc4ad62a110a1c69384e51ed49
Blayne Marjama <blayne.marjama@gmail.com>
2018-01-06 13:39:03 -0800

    implemented reducer tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 import App from '../containers/App';
+
+import reducers from '../reducers'; 
+import dataReducer from '../reducers/dataReducer'
+import * as actions from '../actions'
+import * as types from '../constants';
+
+const store = createStore(reducers)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}>
+		<App />
+	</Provider>, div);
 });
+
+describe('reducer tests', () => {
+  it('should return initial state', () => {
+    expect(
+      dataReducer(undefined, {})
+    )
+    .toEqual(
+      {
+        message: null,
+        data: []
+      }
+    )
+  })
+  
+  it('should handle message change', () => {
+    const message = "Running the test"
+    expect(
+      dataReducer(undefined, {
+        type: types.MESSAGE,
+        payload: message
+      })
+    ).toEqual({
+      message,
+      data: []
+    })
+  })
+
+  it('should handle a form submission', () => {
+    const newUser = { firstName: "bob", lastName: "builder" }
+    expect(
+      dataReducer(undefined, {
+        type: types.SUBMITFORM,
+        payload: newUser
+      })
+    ).toEqual({
+      message: null,
+      data:[
+        newUser
+      ]
+    })
+  })
+})
